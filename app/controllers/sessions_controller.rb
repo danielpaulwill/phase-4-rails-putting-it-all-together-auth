bf2ff16721e9cb9da8e149508dcab3383fbeabab
Daniel Williams <danielpaulwill@gmail.com>
2022-08-02 22:16:30 -0400

    Done.

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,12 @@
+class SessionsController < ApplicationController
+
+  # def create
+  #   user = User.find_by(username: params[:username])
+  #   if user&.authenticate(params[:password])
+  #     puts "yay"
+  #   else
+  #     render json: { errors: user.errors.full_messages }, status: :unprocessable_entity
+  #   end
+  # end
+
+end
